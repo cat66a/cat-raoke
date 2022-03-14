d84395cb2ec38a28c72b591f2d7267ff9f42428a
Cat66000 <36672371+Cat66000@users.noreply.github.com>
2022-03-14 12:57:23 +0100

    refactor(rest)

--- src/rest.ts
+++ src/rest.ts
@@ -1,28 +1,57 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
-import { bot_token, main_guild_id } from "./loadedConfig";
+import { bot_token, guild_ids, main_guild_id } from "./loadedConfig";
 import { commandPropertiesArray } from "./commands";
 
 const rest = new REST({ version: "9" }).setToken(bot_token);
 
 export async function RestLoadingApplicationCommands() {
-  const client_id: string = ((await rest.get("/users/@me")) as any).id;
+  try {
+    const client_id: string = ((await rest.get("/users/@me")) as any).id;
 
-  const commandProperties = commandPropertiesArray();
-  const testCommands = commandProperties.filter((commandProps) =>
-    !commandProps.global
-  );
-  const globalCommands = commandProperties.filter((commandProps) =>
-    commandProps.global
-  );
+    const commandProperties = commandPropertiesArray();
+    const testCommands = commandProperties.filter((commandProps) =>
+      !commandProps.global
+    );
+    const globalCommands = commandProperties.filter((commandProps) =>
+      commandProps.global
+    );
 
-  rest.put(
-    Routes.applicationGuildCommands(client_id, main_guild_id),
-    { body: testCommands.map((cmd) => cmd.APIProperties) },
-  ).catch(console.warn);
+    /* const getGlobalCommands = await rest.get(
+      Routes.applicationCommands(client_id),
+    ) as [{ id: string }];
 
-  rest.put(
-    Routes.applicationCommands(client_id),
-    { body: globalCommands.map((cmd) => cmd.APIProperties) },
-  ).catch(console.warn);
+    await Promise.all(getGlobalCommands.map(async (cmd) => {
+      await rest.delete(
+        Routes.applicationCommand(client_id, cmd.id),
+      );
+    }));
+
+    await Promise.all(guild_ids.map(async (guildId) => {
+      const getGuildCommands = await rest.get(
+        Routes.applicationGuildCommands(client_id, guildId),
+      ) as [{ id: string }];
+
+      await Promise.all(getGuildCommands.map(async (cmd) => {
+        await rest.delete(
+          Routes.applicationGuildCommand(client_id, guildId, cmd.id),
+        );
+      }));
+    }));
+
+    console.log("cc");
+    */
+
+    await rest.put(
+      Routes.applicationGuildCommands(client_id, main_guild_id),
+      { body: testCommands.map((cmd) => cmd.APIProperties) },
+    );
+
+    await rest.put(
+      Routes.applicationCommands(client_id),
+      { body: globalCommands.map((cmd) => cmd.APIProperties) },
+    );
+  } catch (error) {
+    console.warn(error);
+  }
 }
