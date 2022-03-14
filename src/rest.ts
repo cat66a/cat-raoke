c268cec92f9a36d56b9b3871bb90c6057c96854d
Cat66000 <36672371+Cat66000@users.noreply.github.com>
2022-03-14 11:20:27 +0100

    feat(rest): revamp to separate global (prod) commands from test ones

--- src/rest.ts
+++ src/rest.ts
@@ -1,6 +1,6 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
-import { bot_token, guild_ids } from "./loadedConfig";
+import { bot_token, main_guild_id } from "./loadedConfig";
 import { commandPropertiesArray } from "./commands";
 
 const rest = new REST({ version: "9" }).setToken(bot_token);
@@ -8,10 +8,21 @@ const rest = new REST({ version: "9" }).setToken(bot_token);
 export async function RestLoadingApplicationCommands() {
   const client_id: string = ((await rest.get("/users/@me")) as any).id;
 
-  guild_ids.forEach((guild_id) => {
-    rest.put(
-      Routes.applicationGuildCommands(client_id, guild_id),
-      { body: commandPropertiesArray() },
-    ).catch(console.warn);
-  });
+  const commandProperties = commandPropertiesArray();
+  const testCommands = commandProperties.filter((commandProps) =>
+    !commandProps.global
+  );
+  const globalCommands = commandProperties.filter((commandProps) =>
+    commandProps.global
+  );
+
+  rest.put(
+    Routes.applicationGuildCommands(client_id, main_guild_id),
+    { body: testCommands.map((cmd) => cmd.APIProperties) },
+  ).catch(console.warn);
+
+  rest.put(
+    Routes.applicationCommands(client_id),
+    { body: globalCommands.map((cmd) => cmd.APIProperties) },
+  ).catch(console.warn);
 }
