6c4802b1be5add6f574ab1ccb8f0f9b6998de684
Cat66000 <36672371+Cat66000@users.noreply.github.com>
2022-03-22 19:29:02 +0100

    feat: enforce process.cwd to be the root folder of the bot

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-import a from "./built/init.js";
+import {init} from "./built/init.js";
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+process.chdir(__dirname)
+
+await init();
--- src/init.ts
+++ src/init.ts
@@ -5,50 +5,54 @@ import { restLoadApplicationCommands } from "./rest.js";
 
 const { FLAGS } = Intents;
 
-console.log('Loading commands inside "commands" dir');
-await loadCommands();
-console.log("Commands successfully loaded");
+export async function init() {
+  console.log('Loading commands inside "commands" dir');
+  await loadCommands();
+  console.log("Commands successfully loaded");
 
-const client = new Client({
-  intents: [FLAGS.GUILDS, FLAGS.GUILD_VOICE_STATES, FLAGS.GUILD_MEMBERS],
-});
+  const client = new Client({
+    intents: [FLAGS.GUILDS, FLAGS.GUILD_VOICE_STATES, FLAGS.GUILD_MEMBERS],
+  });
 
-client.on("ready", async () => {
-  console.log(`Ready as ${client.user.tag}`);
+  client.on("ready", async () => {
+    console.log(`Ready as ${client.user.tag}`);
 
-  console.log("Started reloading application (/) commands.");
-  try {
-    await client.guilds.fetch();
-    await restLoadApplicationCommands(Array.from(client.guilds.cache.keys()));
-  } catch (err) {
-    console.warn(err);
-  }
-  console.log("Successfully reloaded application (/) commands.");
-});
-
-client.on("interactionCreate", async (interaction) => {
-  try {
-    if (!interaction.isCommand()) return;
-
+    console.log("Started reloading application (/) commands.");
     try {
-      const { commandName } = interaction;
-      if (!commands.has(commandName)) {
-        await interaction.reply(
-          `Une erreur étrange est survenue, je n'ai pas trouvé la commande au nom de ${commandName} dans mon code o_O`,
-        );
-      }
-
-      console.log(commandName);
-      const command = commands.get(commandName);
-
-      await command.preExec(interaction);
-    } catch (error) {
-      await interactionErrorHandler(interaction, error);
+      await client.guilds.fetch();
+      await restLoadApplicationCommands(Array.from(client.guilds.cache.keys()));
+    } catch (err) {
+      console.warn(err);
     }
-  } catch (error2) {
-    console.warn(error2);
-  }
-});
+    console.log("Successfully reloaded application (/) commands.");
+  });
+
+  client.on("interactionCreate", async (interaction) => {
+    try {
+      if (!interaction.isCommand()) return;
+
+      try {
+        const { commandName } = interaction;
+        if (!commands.has(commandName)) {
+          await interaction.reply(
+            `Une erreur étrange est survenue, je n'ai pas trouvé la commande au nom de ${commandName} dans mon code o_O`,
+          );
+        }
+
+        console.log(commandName);
+        const command = commands.get(commandName);
+
+        await command.preExec(interaction);
+      } catch (error) {
+        await interactionErrorHandler(interaction, error);
+      }
+    } catch (error2) {
+      console.warn(error2);
+    }
+  });
+
+  await client.login(botToken);
+}
 
 async function interactionErrorHandler(
   interaction: CommandInteraction,
@@ -60,7 +64,3 @@ async function interactionErrorHandler(
 
   console.warn(error);
 }
-
-await client.login(botToken);
-
-export default {};
