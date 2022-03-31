7bde9874e42e4982241441ee148c46e55dadbfcd
Cat66000 <36672371+Cat66000@users.noreply.github.com>
2022-03-31 23:50:25 +0200

    feat: world invasion started i also deleted useless/buggy code in index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 import { init } from "./built/init.js";
 import { handleCLIFlags } from "./built/CLI.js";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-process.chdir(__dirname);
 
 handleCLIFlags();
 
 await init();
+console.log("World invasion started")
